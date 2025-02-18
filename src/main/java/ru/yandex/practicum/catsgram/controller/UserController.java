f3936b013209da32aca72cc2a7d2f983273bde44
AlexanderChum <Senneiko@yandex.ru>
2025-02-18 18:43:49 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,75 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findall(){
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Email не может быть пустым");
+        }
+        for (User userFromMemory : users.values()) {
+            if (userFromMemory.getEmail().equals(user.getEmail())) {
+                throw new DuplicatedDataException("Этот имейл уже используется");
+            }
+        }
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        if (user.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (!users.containsKey(user.getId())) {
+            throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден");
+        }
+        for (User userFromMemory : users.values()) {
+            if (userFromMemory.getEmail().equals(user.getEmail())) {
+                throw new DuplicatedDataException("Этот имейл уже используется");
+            }
+        }
+        if (user.getEmail() == null || user.getEmail().isBlank() ||
+            user.getPassword() == null || user.getPassword().isBlank()) {
+            throw new InputMismatchException("Введены неверные данные, попробуйте снова");
+        } else {
+            User oldUser = users.get(user.getId());
+            oldUser.setEmail(user.getEmail());
+            oldUser.setPassword(user.getPassword());
+            return oldUser;
+        }
+    }
+
+    // вспомогательный метод для генерации идентификатора нового поста
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
